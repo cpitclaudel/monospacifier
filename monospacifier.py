0476f866c46de6908886a257eda89f9da117d108
Clément Pit--Claudel <clement.pitclaudel@live.com>
2015-12-06 02:00:31 +0100

    Add merging capabilities

--- monospacifier.py
+++ monospacifier.py
@@ -18,7 +18,12 @@
 # $ fc-list | grep -i symbola
 # /usr/share/fonts/truetype/ttf-ancient-scripts/Symbola605.ttf: Symbola:style=Regular
 
-"""Convert a variable-width font to monospace."""
+"""Convert a variable-width font to monospace, optionally merging it with an
+existing monospace font to extend its unicode coverage."""
+
+# Techincal note: two cycles of (open, modify, save, close) cause a segfault.
+# On the other hand, copying a font and reading, modifying, and overwriting
+# the copy works.
 
 from __future__ import division
 
@@ -26,8 +31,8 @@ import argparse
 import itertools
 import math
 import os
-import sys
-from cStringIO import StringIO
+import shutil
+import re
 from collections import Counter
 
 try:
@@ -145,33 +150,17 @@ class FontScaler(object):
         """
         Adjust width of glyphs in using SCALER.
         """
-        print("> Setting width to {}".format(scaler.cell_width))
-
-        counter = Counter()
+        # counter = Counter()
         for glyph in self.font.glyphs():
-            # if glyph.unicode > 0:
-            #     print(unichr(glyph.unicode))
             scaler.scale(glyph)
-            counter[glyph.width] += 1
-
-        print("> Final width distribution: {}".format(", ".join(map(str, counter.most_common(10)))))
-
-    def rename(self, reference):
-        self.font.fontname = "{}-monospacified-for-{}".format(self.font.fontname, reference.fontname)
-        self.font.familyname = "{} monospacified for {}".format(self.font.familyname, reference.familyname)
-        self.font.fullname = "{} monospacified for {}".format(self.font.fullname, reference.fullname)
-        # font.sfnt_names = [(lng, key, (val if newname in val
-        #                                else val.replace(oldname, newname)))
-        #                    for (lng, key, val) in font.sfnt_names]
-        # print("\n".join("{}: {}".format(attr, getattr(font,attr)) for attr in dir(font)))
+            # counter[glyph.width] += 1
+        # print("> Final width distribution: {}".format(", ".join(map(str, counter.most_common(10)))))
 
     def write(self, name):
         """
-        Save the font to NAME.
+        Save font to NAME.
         """
-        name += ".ttf"
         self.font.generate(name)
-        print("> Wrote {}".format(name))
 
 def plot_widths(glyphs):
     # pylint: disable=unused-variable
@@ -183,6 +172,50 @@ def plot_widths(glyphs):
     pyplot.hist(widths, bins=400)
     pyplot.show()
 
+def fname(path):
+    return os.path.splitext(os.path.basename(path))[0]
+
+def make_monospace(ref, fnt, save_to):
+    reference = fontforge.open(ref)
+    width = FontScaler.most_common_width(reference)
+
+    output = os.path.join(save_to, "{}-monospacified-for-{}.ttf".format(fname(fnt), fname(ref)))
+    print("> * [{}]({}) monospacified for **{}** (width: {})".format(fname(fnt), output, fname(ref), width))
+
+    shutil.copy(fnt, output)
+    fscaler = FontScaler(output)
+
+    fscaler.font.fontname = "{}-monospacified-for-{}".format(fscaler.font.fontname, reference.fontname)
+    fscaler.font.familyname = "{} monospacified for {}".format(fscaler.font.familyname, reference.familyname)
+    fscaler.font.fullname = "{} monospacified for {}".format(fscaler.font.fullname, reference.fullname)
+    # fscaler.font.sfnt_names = [(lng, key, (val if newname in val
+    #                                else val.replace(oldname, newname)))
+    #                    for (lng, key, val) in font.sfnt_names]
+    # print("\n".join("{}: {}".format(attr, getattr(font,attr)) for attr in dir(font)))
+
+    gscaler = StretchingGlyphScaler(width, FontScaler.average_width(fscaler.font))
+    fscaler.scale_glyphs(gscaler)
+    fscaler.write(output)
+    return output
+
+def cleanup_font_name(name):
+    return re.sub('.monospacified.for.*', '', name)
+
+def merge_fonts(ref, fnt, save_to):
+    output = os.path.join(save_to, "{}-extended-with-{}.ttf".format(fname(ref), cleanup_font_name(fname(fnt))))
+    # print("> * [{}]({}) extended with **{}**".format(fname(ref), output, cleanup_font_name(fname(fnt))))
+
+    shutil.copy(ref, output)
+    fallback = fontforge.open(fnt)
+    reference = fontforge.open(output)
+    reference.fontname = "{}-extended-with-{}".format(reference.fontname, cleanup_font_name(fallback.fontname))
+    reference.familyname = "{} extended with {}".format(reference.familyname, cleanup_font_name(fallback.familyname))
+    reference.fullname = "{} extended with {}".format(reference.fullname, cleanup_font_name(fallback.fullname))
+
+    reference.mergeFonts(fnt)
+    reference.generate(output)
+    return output
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--references', required="True", nargs='+',
@@ -190,32 +223,19 @@ def parse_arguments():
                         "The metrics (character width, ...) of the newly created monospace fonts are inherited from these.")
     parser.add_argument('--inputs', required="True", nargs='+',
                         help="Variable-width fonts to monospacify.")
-    parser.add_argument('--save-to', default=".", help="Where to save the newly generated monospace fonts. Defaults to current directory.")
+    parser.add_argument('--save-to', default=".",
+                        help="Where to save the newly generated monospace fonts. Defaults to current directory.")
+    parser.add_argument('--merge', action='store_const', const=True, default=False,
+                        help="Whether to create a copy of the reference, including monospacified glyphs from the inputs.")
     return parser.parse_args()
-
-def fname(path):
-    return os.path.splitext(os.path.basename(path))[0]
 
 def main():
     args = parse_arguments()
 
     for ref, fnt in itertools.product(args.references, args.inputs):
-        fscaler = FontScaler(fnt)
-        reference = fontforge.open(ref)
-
-        width = FontScaler.most_common_width(reference)
-        gscaler = StretchingGlyphScaler(width, FontScaler.average_width(fscaler.font))
-        fscaler.scale_glyphs(gscaler)
-
-        output = os.path.join(args.save_to, "{}-monospacified-for-{}".format(fname(fnt), fname(ref)))
-        print("* [{} Monospace]({}) for **{}** (width: {})".format(fname(fnt), output, fname(ref), width))
-        fscaler.rename(reference)
-        fscaler.write(output)
-        fscaler.font.close()
-
-    # plot_widths(fscaler.font.glyphs())
-    # gscaler = BasicGlyphScaler(FontScaler.most_common_width(reference))
-    # gscaler = AllowWideCharsGlyphScaler(FontScaler.most_common_width(reference), FontScaler.average_width(fscaler.font))
+        output = make_monospace(ref, fnt, args.save_to)
+        if args.merge:
+            merge_fonts(ref, output, args.save_to)
 
 if __name__ == '__main__':
     main()
